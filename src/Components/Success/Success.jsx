ccf7f5338ba754deb90b1ef42ff1c46e03983769
Md-Shakib-Akando <shakibakando2@gmail.com>
2025-04-22 16:52:48 +0600

    reachart is added and fully work

--- src/Components/Success/Success.jsx
+++ src/Components/Success/Success.jsx
@@ -35,8 +35,8 @@ const Success = () => {
                 <div className='grid grid-cols-1 md:grid-cols-4 gap-6 mb-6'>
 
                    
-                    <div className='bg-[#0F0F0F05] border border-[#0F0F0F20] w-9/12 md:w-11/12 rounded-2xl'>
-                        <div className='p-8 ml-8'>
+                    <div className=' bg-[#0F0F0F05] border border-[#0F0F0F20] w-9/12 ml-14 md:ml-0  rounded-2xl'>
+                        <div className='flex flex-col justify-center items-center p-8 '>
                             <img src={CardImg1} alt="" />
                             <h1 className='text-4xl font-bold mt-5 mb-3'>
                                 {startCount && <CountUp start={0} end={199} duration={2} />}+
@@ -46,8 +46,8 @@ const Success = () => {
                     </div>
 
                     
-                    <div className='bg-[#0F0F0F05] border border-[#0F0F0F20] w-9/12 md:w-11/12 rounded-2xl'>
-                        <div className='p-8 ml-8'>
+                    <div className='bg-[#0F0F0F05] border border-[#0F0F0F20] w-9/12 ml-14 md:ml-0  rounded-2xl'>
+                        <div className='flex flex-col justify-center items-center p-8 '>
                             <img src={CardImg3} alt="" />
                             <h1 className='text-4xl font-bold mt-5 mb-3'>
                                 {startCount && <CountUp start={0} end={467} duration={2} />}+
@@ -57,8 +57,8 @@ const Success = () => {
                     </div>
 
                    
-                    <div className='bg-[#0F0F0F05] border border-[#0F0F0F20] w-9/12 md:w-11/12 rounded-2xl'>
-                        <div className='p-8 ml-8'>
+                    <div className='bg-[#0F0F0F05] border border-[#0F0F0F20] w-9/12 ml-14 md:ml-0   rounded-2xl'>
+                        <div className='flex flex-col justify-center items-center p-8 '>
                             <img src={CardImg2} alt="" />
                             <h1 className='text-4xl font-bold mt-5 mb-3'>
                                 {startCount && <CountUp start={0} end={1900} duration={2} />}+
@@ -68,8 +68,8 @@ const Success = () => {
                     </div>
 
                     
-                    <div className='bg-[#0F0F0F05] border border-[#0F0F0F20] w-9/12 md:w-11/12 rounded-2xl'>
-                        <div className='p-8 ml-8'>
+                    <div className='bg-[#0F0F0F05] border border-[#0F0F0F20] w-9/12 ml-14 md:ml-0  rounded-2xl'>
+                        <div className='flex flex-col justify-center items-center p-8 '>
                             <img src={CardImg4} alt="" />
                             <h1 className='text-4xl font-bold mt-5 mb-3'>
                                 {startCount && <CountUp start={0} end={300} duration={2} />}+
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -3,7 +3,7 @@ import { getBookMark, removeBookMark } from '../../utilites';
 import Book from './Book';
 import { Link } from 'react-router';
 
-
+import { BarChart, Bar, XAxis, YAxis, Cell, ResponsiveContainer } from 'recharts';
 const Bookings = () => {
     const [displayData, setDisplayData] = useState([])
     useEffect(() => {
@@ -15,7 +15,43 @@ const Bookings = () => {
         setDisplayData(getBookMark());
 
     }
+    const getPath = (x, y, width, height) => (
+        `M${x},${y + height}
+         C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2}, ${y}
+         C${x + width / 2},${y + height / 3} ${x + 2 * width / 3},${y + height} ${x + width}, ${y + height}
+         Z`
+    );
+
+    const TriangleBar = (props) => {
+        const {
+            fill, x, y, width, height,
+        } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
+
+
+
+    const colors = ['#0088FE', '#FFBB28', '#00C49F', '#FF8042'];
+    const CustomLabel = (props) => {
+        const { x, y, width, value, index } = props;
+        const fillColor = colors[index % colors.length];
+
+        return (
+            <text
+                x={x + width / 2}
+                y={y - 10}
+                fill={fillColor}
+                textAnchor="middle"
+                fontSize={16}
+                fontWeight="bold"
+            >
+                {value}
+            </text>
+        );
+    };
     return (
+
         <>
             {
                 displayData.length === 0 ? (
@@ -24,13 +60,37 @@ const Bookings = () => {
                         <p className='text-sm md:text-lg text-center'>Our platform connects you with verified, experienced Lawyers across various specialties — all at your convenience.</p>
 
                         <Link to='/' class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-blue-600 border border-blue-700 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" data-rounded="rounded-md" data-primary="blue-600" data-primary-reset="{}">
-                           Book An Appointment
+                            Book An Appointment
                         </Link>
                     </div>
                 ) : (
-                    displayData.map(data => (
-                        <Book key={data.id} data={data} handleDelete={handleDelete} />
-                    ))
+
+                    <>
+                        <div className='pt-4 border border-[#0F0F0F20] rounded-xl mb-10'>
+                            <div className="w-full flex justify-center mb-10 overflow-x-auto">
+                                <div className="w-[600px] md:w-[2000px] h-[400px] md:h-[500px]">
+                                    <ResponsiveContainer width="100%" height="100%">
+                                        <BarChart data={displayData}>
+                                            <XAxis dataKey="name" />
+                                            <YAxis />
+                                            <Bar dataKey="fee" shape={<TriangleBar />} label={CustomLabel}>
+                                                {displayData.map((entry, index) => (
+                                                    <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                                                ))}
+                                            </Bar>
+                                        </BarChart>
+                                    </ResponsiveContainer>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        {
+                            displayData.map(data => (
+                                <Book key={data.id} data={data} handleDelete={handleDelete} />
+                            ))
+                        }
+                    </>
                 )
             }
         </>
